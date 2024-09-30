914ccb0139185dc581d08f3cfdc3eed8ea1e26e8
Harsh-Kumar3813 <130229476+Harsh-Kumar3813@users.noreply.github.com>
2024-10-01 02:37:36 +0530

    Update TodoSection.js

--- frontend/app/components/todo/TodoSection.js
+++ frontend/app/components/todo/TodoSection.js
@@ -1,17 +1,13 @@
+import TodoList from './TodoList';
+import styles from '../../styles/Todo.module.css';
 
-import styles from '../../styles/Todo.module.css'
-import TodoList from './TodoList'
-
-const TodoSection = ({ title, todos, action }) => {
+export default function TodoSection({ title, todos = [], action }) {
     return (
         <div className={styles.todoSection}>
             <h1 className="title">
                 {title} - {todos.length}
             </h1>
-
             <TodoList todos={todos} action={action} />
         </div>
-    )
+    );
 }
-
-export default TodoSection
